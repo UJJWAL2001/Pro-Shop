e196c9636a9f84ec011725523561305e7da6d065
UJJWAL CHAUDHARY <ujjwalanju2001@gmail.com>
2021-07-20 19:47:00 +0530

    backend linked to frontend

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const app = express()
+const dotenv = require('dotenv')
+
+dotenv.config()
 
 const products = require('./data/products')
 
@@ -16,6 +19,8 @@ app.get('/api/products/:id', (req, res) => {
   res.json(product)
 })
 
-app.listen(5000, () => {
-  console.log('Listening to port 5000...')
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+  console.log(`Server runnning in ${process.env.NODE_ENV} mode on port ${PORT}`)
 })
