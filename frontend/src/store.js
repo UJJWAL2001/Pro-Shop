4297e06f6513985e10d3a71e4384be9285eec51b
UJJWAL CHAUDHARY <ujjwalanju2001@gmail.com>
2021-07-23 12:35:13 +0530

    redux-react product details

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,10 +1,14 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
-import { productListReducer } from './reducres/productReducers'
+import {
+  productListReducer,
+  productDetailsReducer,
+} from './reducres/productReducers'
 
 const reducer = combineReducers({
   productList: productListReducer,
+  productDetails: productDetailsReducer,
 })
 
 const initialState = {}
