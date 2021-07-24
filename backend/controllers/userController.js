6b1a9f698ef5f70bee4dba206482ea690eb700f4
UJJWAL CHAUDHARY <ujjwalanju2001@gmail.com>
2021-07-24 16:26:44 +0530

    auth completed -backend

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -30,7 +30,7 @@ const authUser = asyncHandler(async (req, res) => {
 const getUserProfile = asyncHandler(async (req, res) => {
   const user = await User.findById(req.user.id)
   if (user) {
-    res.json({
+    res.send({
       id: user._id,
       name: user.name,
       email: user.email,
@@ -42,4 +42,33 @@ const getUserProfile = asyncHandler(async (req, res) => {
   }
 })
 
-export { authUser, getUserProfile }
+// @desc    Create new usern
+// @route   POST /api/user
+// @access  Public
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body
+
+  const userExist = await User.findOne({ email })
+
+  if (userExist) {
+    res.status(400)
+    throw new Error('User Already Exists')
+  }
+
+  const user = await User.create({ name, email, password })
+
+  if (user) {
+    res.status(201).json({
+      id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    })
+  } else {
+    res.status(400)
+    throw new Error('Invalid User data')
+  }
+})
+
+export { authUser, registerUser, getUserProfile }
--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -1,7 +1,7 @@
 const notFound = (req, res, next) => {
-  const error = new Error(`Not found - ${req.originalUrl}`)
-  res.status(404)
-  next(error)
+  // const error = new Error(`Not found - ${req.originalUrl}`)
+  // res.status(404)
+  // next(error)
 }
 
 const errorHandler = (err, req, res, next) => {
