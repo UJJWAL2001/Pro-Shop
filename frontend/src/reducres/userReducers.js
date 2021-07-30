2c08de9c75d276012b9be7dfcf80e40253554359
UJJWAL CHAUDHARY <ujjwalanju2001@gmail.com>
2021-07-30 18:01:26 +0530

    orders for admin

--- frontend/src/reducres/userReducers.js
+++ frontend/src/reducres/userReducers.js
@@ -29,7 +29,7 @@ import {
 export const userLoginReducer = (state = { userInfo: {} }, action) => {
   switch (action.type) {
     case USER_LOGIN_REQUEST:
-      return { userInfo: {}, loading: true }
+      return { ...state, loading: true }
     case USER_LOGIN_SUCCESS:
       return { loading: false, userInfo: action.payload }
     case USER_LOGIN_FAIL:
