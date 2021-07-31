838ae2e2900b136b15c386c241b79da39e475253
UJJWAL CHAUDHARY <ujjwalanju2001@gmail.com>
2021-07-31 16:03:34 +0530

    carousel for top rated products

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -5,7 +5,7 @@ import Product from '../models/productModel.js'
 // @route   GET /api/products
 // @access  Public
 const getProducts = asyncHandler(async (req, res) => {
-  const pageSize = 2
+  const pageSize = 10
   const page = Number(req.query.pageNumber) || 1
 
   const keyword = req.query.keyword
@@ -36,6 +36,15 @@ const getProductById = asyncHandler(async (req, res) => {
     res.status(404)
     throw new Error('Product not Found')
   }
+})
+
+// @desc    Fetch to rated product
+// @route   GET /api/products/top
+// @access  Public
+const getTopProducts = asyncHandler(async (req, res) => {
+  const products = await Product.find({}).sort({ rating: -1 }).limit(3)
+
+  res.json(products)
 })
 
 // @desc    Delete  product by id
@@ -146,4 +155,5 @@ export {
   createProduct,
   updateProduct,
   createProductReview,
+  getTopProducts,
 }
